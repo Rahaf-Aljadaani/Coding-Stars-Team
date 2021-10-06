dbde8b76205540e80066b82106d8a4f9627e36bb
Ashish Pisey <ashishpisey21@gmail.com>
2021-10-06 22:47:21 +0530

    navigation controller added
    tab bar with navigation controller

--- Tabbar Navigation/Tabbar Navigation/ViewController.swift
+++ Tabbar Navigation/Tabbar Navigation/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextAction(_ sender: UIButton) {
+        let detailVC = storyboard?.instantiateViewController(withIdentifier: "Detail") as! DetailViewController
+        self.navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
 }
 
